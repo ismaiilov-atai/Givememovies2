7ebc20121ed53ff67deed3d86ff8b698dddb3db3
Carlos Bárcena Bescansa <carlos.barcena.bescansa@gmail.com>
2023-08-09 16:59:34 +0200

    styled some front end and watchlist button

--- client/src/components/MovieList/index.js
+++ client/src/components/MovieList/index.js
@@ -3,6 +3,11 @@ import './MovieList.css';
 
 const MovieList = ({ movies, setSelectedMovie, isLoading, error, hasSearched }) => {
 
+  const handleWatchList = (movie) => {
+    // TO DO!!!!!!!
+    console.log("Add to watch-list:", movie.title);
+  }
+
   if (isLoading) {
       return <div>Loading...</div>;
   }
@@ -19,6 +24,14 @@ const MovieList = ({ movies, setSelectedMovie, isLoading, error, hasSearched }) 
       <ul className='list'>
           {movies.map(movie => (
               <div className='movie' key={movie.id} onClick={() => setSelectedMovie(movie)}>
+                  <button
+                          className="watch-list-btn"
+                          onClick={(e) => {
+                              e.stopPropagation();
+                              handleWatchList(movie);
+                          }}>
+                          Watch Later
+                      </button>
                   <h3 className='title'>{movie.title}</h3>
                   {movie.poster_path ? (
                       <img className='img' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie poster" />
